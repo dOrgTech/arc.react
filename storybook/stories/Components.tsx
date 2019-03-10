2e7013bcf1fe99586ff57be831c53ba75e237ec8
dOrgJelli <jelli@dorg.tech>
2019-03-10 01:37:13 -0800

    component prop editors

--- storybook/stories/Components.tsx
+++ storybook/stories/Components.tsx
@@ -1,14 +1,23 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import ContainerView from "../helpers/ContainerView";
+import ContainerView, { PropTypes } from "../helpers/ContainerView";
 import DAO from "../../src/containers/DAO";
 
 export default () => 
   storiesOf("Components", module)
-    .add("DAO", () =>
-      <ContainerView 
-        name={ "DAO" }
-        Type={ DAO }
-        // TODO: load these addresses from the graphnode
-        props={{ address: "0xcB4e66eCA663FDB61818d52A152601cA6aFEf74F" }} />
-    );
+    .add("DAO", () => {
+      return (
+        <ContainerView
+          name={ "DAO" }
+          Component={ DAO }
+          // TODO: load these addresses from the graphnode
+          propEditors={[
+            {
+              friendlyName: "DAO Address",
+              name: "address",
+              defaultValue: "0xcB4e66eCA663FDB61818d52A152601cA6aFEf74F",
+              type: PropTypes.string
+            }
+          ]} />
+      )
+    });
