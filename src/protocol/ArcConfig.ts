281bf555f19d31250bcfe1a9a29d59a549286e35
shivani <gupta.shivani254@gmail.com>
2019-05-09 12:54:51 +0530

    add contract introspection

--- src/protocol/ArcConfig.ts
+++ src/protocol/ArcConfig.ts
@@ -1,5 +1,7 @@
 import { ProtocolConfig } from "../runtime/ProtocolConfig";
-import { Arc as Connection } from "@daostack/client";
+import { Arc as Connection} from "@daostack/client";
+
+const deployedContractAddresses = require("@daostack/migration/migration.json");
 
 export class ArcConfig extends ProtocolConfig
 {
@@ -8,13 +10,19 @@ export class ArcConfig extends ProtocolConfig
   constructor(
     public web3HttpUrl: string,
     public graphqlHttpUrl: string,
-    public graphqlWsUrl: string
+    public graphqlWsUrl: string,
+    public ipfsProvider: any,
+    public network: string
   ) {
     super();
     this.connection = new Connection({
       graphqlHttpProvider: graphqlHttpUrl,
       graphqlWsProvider: graphqlWsUrl,
-      web3Provider: web3HttpUrl
+      web3Provider: web3HttpUrl,
+      ipfsProvider: ipfsProvider,
+      contractAddresses: {
+        ...deployedContractAddresses[network]
+      }.base
     });
   }
 }
@@ -26,5 +34,20 @@ export class ArcConfig extends ProtocolConfig
 export const DefaultArcConfig = new ArcConfig(
   "http://127.0.0.1:8545",
   "http://127.0.0.1:8000/subgraphs/name/daostack",
-  "ws://127.0.0.1:8001/subgraphs/name/daostack"
+  "ws://127.0.0.1:8001/subgraphs/name/daostack",
+  "localhost",
+  "private"
+);
+
+export const HostedArcConfig = new ArcConfig(
+  "wss://mainnet.infura.io/ws/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2",
+  "https://subgraph.daostack.io/subgraphs/name/v16-without-uc",
+  "wss://ws.subgraph.daostack.io/subgraphs/name/v16-without-uc",
+  {
+      "host": "subgraph.daostack.io",
+      "port": "443",
+      "protocol": "https",
+      "api-path": "/ipfs/api/v0/"
+  },
+  "mainnet"
 );
--- storybook/stories/Environment.tsx
+++ storybook/stories/Environment.tsx
@@ -2,14 +2,105 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { setupGraphiQL } from "@storybook/addon-graphql";
 import QuerySnippets from "../helpers/QuerySnippets";
+import {
+  Arc,
+  ArcConfig,
+  DefaultArcConfig } from "../../src";
+import {
+  ObjectInspector,
+  ObjectRootLabel,
+  ObjectLabel
+} from "react-inspector";
+import {
+  Typography,
+} from '@material-ui/core';
 
+const R = require('ramda')
+const objectInspector = (data: any, name: string, tooltip: string) => {
+  return (
+    <ObjectInspector
+      data={{...data}}
+      name={name}
+      expandLevel={1}
+      theme={"chromeDark"}
+      nodeRenderer={(node: any) => nodeRenderer(node, tooltip)} />
+  );
+}
+
+const nodeRenderer = (node: any, tooltip: string) => {
+  const { depth, name, data, isNonenumerable } = node;
+
+  return (
+    depth === 0
+      ? <ObjectRootLabel name={name} data={tooltip} />
+      : <ObjectLabel name={name} data={data} isNonenumerable={isNonenumerable} />
+  );
+}
 // TODO: make configurable
+const defaultAddress = "0x0000000000000000000000000000000000000000"
 const graphiql = setupGraphiQL({ url: "http://127.0.0.1:8000/subgraphs/name/daostack" });
 
 export default () =>
   storiesOf("Environment", module)
-    .add("Connections", () => <div>TODO: connection statuses (eth_node, contracts, graph_node)</div>)
-    .add("Contracts", () => <div>TODO: contracts deployed</div>)
+    .add("Connections", () => {
+      return (
+        <Arc config={DefaultArcConfig}>
+          <Arc.Config>
+            {(arc: ArcConfig) => {
+              return (
+                <div>
+                  <Typography variant="h6" component="h6">
+                    Connection Status
+                  </Typography>
+                  {objectInspector(arc.connection, "arc.connection", "Connection Status")}
+                </div>
+              )
+            }}
+          </Arc.Config>
+        </Arc>
+      )})
+    .add("Contracts", () => {
+      return (
+        <Arc config={DefaultArcConfig}>
+          <Arc.Config>
+            {(arc: ArcConfig) => {
+              let render: Array<any> = []
+              R.forEachObjIndexed( (value: any, key: any) => {
+                arc.connection.setAccount(defaultAddress)
+                try {
+                  let contract = arc.connection.getContract(key)
+                  let methods = {}
+                  R.forEachObjIndexed((value: any, key: any) => {
+                    if (/[a-b]*\(/.test(key)) methods[key] = value
+                  }, contract.methods)
+                  contract.methods = methods
+                  render.push({'key': key, 'contract': contract})
+                } catch (e) {
+                  console.log(e.message)
+                }
+              }, arc.connection.contractAddresses)
+
+              return (
+                <div>
+                  <Typography variant="h4" component="h4">
+                    Contract txMethods, viewMethods and properties
+                  </Typography>
+                  {render.map(o => {
+                    return (
+                      <>
+                        <Typography variant="h6" component="h6">
+                          {`${o['key']} Contract`}
+                        </Typography>
+                        {objectInspector(o['contract'].methods, `${o['key']}Contract.methods`, `${o['key']} Contract Methods`)}
+                      </>
+                    )
+                  })}
+                </div>
+              )
+            }}
+          </Arc.Config>
+        </Arc>
+      )})
     .add("The Graph", graphiql(
       `{
         daos {
