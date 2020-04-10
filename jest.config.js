da1fe056b2e5058b6e65931b192a989b1979eead
cbrzn <cesarbrazon10@gmail.com>
2020-04-10 04:18:56 +0200

    test passes, cleaning some dependencies

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  transform: {
+    ".(ts|tsx)$": "ts-jest"
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/test/declarations.d.ts"],
+  testRegex: "(/test/.*(spec).*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleNameMapper: {
+    "\\.(css)$": "identity-obj-proxy"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
